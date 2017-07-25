155f7b52e598bab00edb487363c346677dedd5e4
mplibunao <markpaololibunao@gmail.com>
2017-07-26 04:48:56 +0800

    Test helper revisited: Set up renderComponent function which is incharge of creating an instance of the component, rendering it into the fake DOM and wrapping it inside a jquery object

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,21 @@
 import jsdom from 'jsdom';
+// preprend _ so that jquery doesn't try to access DOM browser (which doesn't exist) and fail
+import _$ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 
 
 // Setting up testing environment to run like a browser in the CLI
 // Assign a fake html DOM (using jsdom) to the global.document (window)
 global.document =  jsdom.jsdom('<!DOCTYPE HTML><HTML><BODY></BODY></HTML>');
 global.window =  global.document.defaultView;
+// Wrap global.window in a jquery object
+const $ = _$(global.window);
+
+
+// Build 'renderComponent' helper that renders a given react component
+function renderComponent(Component){
+  const componentInstance = TestUtils.renderIntoDocument(<Component />);
+  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML which is wrapped inside a jquery object
+
+}
