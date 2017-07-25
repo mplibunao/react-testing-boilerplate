b845e97f90f3ebc74f51dc91af346da5ea08bbff
mplibunao <markpaololibunao@gmail.com>
2017-07-26 02:37:06 +0800

    Added tests for action creator 'saveComment'

---
+++ test/actions/index_test.js
@@ -0,0 +1,21 @@
+import { expect } from '../test_helper';
+import { SAVE_COMMENT } from '../../src/actions/types';
+import { saveComment} from '../../src/actions/';
+
+describe('Actions', ()=>{
+  describe('saveComment', ()=>{
+    let action;
+
+    beforeEach(()=>{
+      action = saveComment('New Comment');
+    });
+
+    it('has the correct type', ()=>{
+      expect(action.type).to.equal(SAVE_COMMENT);
+    });
+
+    it('has the correct payload', ()=>{
+      expect(action.payload).to.equal('New Comment');
+    });
+  });
+});
